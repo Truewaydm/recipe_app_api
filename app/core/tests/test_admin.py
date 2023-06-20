8f331142348ed2586f1739418cffdc63f24318bf
Rainbowdm <dim8035@gmail.com>
2023-06-20 17:40:15 +0300

    Setup postgres

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -8,17 +8,17 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@lol.com',
+            email='admin@test.com',
             password='password123'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='test@test.com',
+            email='test@test1.com',
             password='password123',
             name='Test user full name'
         )
 
-    def test_user_listed(self):
+    def test_users_listed(self):
         """Test that users are listed on user page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
@@ -28,15 +28,14 @@ class AdminSiteTests(TestCase):
 
     def test_user_change_page(self):
         """Test that the user edit page works"""
-        # /admin/core/user/1
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
-        self.assertEquals(res.status_code, 200)
+        self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
         """Test that the create user page works"""
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
-        self.assertEquals(res.status_code, 200)
+        self.assertEqual(res.status_code, 200)
