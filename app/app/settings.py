c7c9812ab857b1d9f9f3f56c89deab532c9fb7dc
Rainbowdm <dim8035@gmail.com>
2023-06-20 20:13:52 +0300

    Added endpoint for creating users

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ibz1d_27)0x%!%_6pa)hwex5gmxn$ljpc!1whrmlj!tu&9aauc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -36,7 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
